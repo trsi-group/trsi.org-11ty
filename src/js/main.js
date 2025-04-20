f98863a42cf89db1a62e5260ec9594be3b2500d9
oliver <gokohl@gmail.com>
2025-04-20 22:39:10 +0200

    filters added

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,38 @@
+function handleFilterChange(event) {
+  const cards = document.querySelectorAll("#feed-wrapper .column");
+  const typeFilter = document.getElementById("TypeFilter");
+  const platformFilter = document.getElementById("PlatformFilter");
+
+  const selectedType = typeFilter.value;
+  const selectedPlatform = platformFilter.value;
+  cards.forEach(card => {
+    const typeMatch = !selectedType || card.dataset.type === selectedType;
+    const platformMatch = !selectedPlatform || card.dataset.platform === selectedPlatform;
+  
+    const matches = typeMatch && platformMatch;
+    card.style.display = matches ? "" : "none";
+  })
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log("main.js: DOMContentLoaded");
 
-  // Get all "navbar-burger" elements
+  // register nav menu
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Add a click event on each of them
   $navbarBurgers.forEach( el => {
     el.addEventListener('click', () => {
-
-      // Get the target from the "data-target" attribute
       const target = el.dataset.target;
       const $target = document.getElementById(target);
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       el.classList.toggle('is-active');
       $target.classList.toggle('is-active');
-
     });
   });
+
+  // register filter components
+  const typeFilter = document.getElementById("TypeFilter");
+  typeFilter.addEventListener("change", handleFilterChange);
+  
+  const platformFilter = document.getElementById("PlatformFilter");
+  platformFilter.addEventListener("change", handleFilterChange);  
 });
