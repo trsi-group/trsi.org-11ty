ca6c62938c65c39f1497212cabdaf91b30aa1ef3
oliver <gokohl@gmail.com>
2025-07-04 17:46:11 +0200

    Update site.js
    - og:image fix for homepage

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,7 +6,7 @@ const metadata = {
   title: "Tristar & Red Sector Demo Group - The Sleeping Gods",
   description: "Tristar & Red Sector (TRSI) is a demogroup formed in 1990 from the union of the Commodore 64 warez group Red Sector and Tristar. Known for their intros and demos on C64, Amiga, PC, and consoles, TRSI has left a lasting legacy in demo scene art, code, and music.",
   keywords: "TRSI, Tristar and Red Sector, Demoscene, Amiga demo group, C64, Amiga, Intro, Music, Tracker, Pixel Graphics",
-  logo: "https://trsi.org/img/trsi-logo.png",
+  logo: "/img/trsi-logo.png",
 };
 
 const featureFlags = {
